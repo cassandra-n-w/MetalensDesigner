b29ff8c52c5eb88186366e872d1d2b8bfb473ada
CASSIES-COMPUTE\Cassie <cassie.whits@gmail.com>
2021-07-14 12:43:30 -0700

    polished comparison between simulations. very good agreement

--- Test20GHz.m
+++ Test20GHz.m
@@ -20,12 +20,17 @@ hornfunc(0,0,20e9);
 tempsim = Simulation(lensmodel, f0, f0);
 
 
-scanpos = (-25*tempsim.dy:tempsim.dy:25*tempsim.dy);
+scanpos = (-15*tempsim.dy:tempsim.dy:15*tempsim.dy);
 r = sqrt((tempsim.x).^2 + (tempsim.y).^2);
 mask = (r < (lensmodel.diameter/2));
 
-tempsim.initialize_E_field(hornfunc);
-ideal_coupling = mag2db(abs(tempsim.receiver_scan(hornfunc, 0, scanpos)));
+% according to quasioptics by goldsmith, 7.4: Beam Radius, 
+% a tapered aperture (such as our horn) has best coupling to the gaussian
+% mode with w0/a = 0.35;
+w0 = a1 .* 0.35;
+gaussfunc = @(x,y,f) exp(-(x.^2 + y.^2) / w0^2);
+tempsim.initialize_E_field(gaussfunc);
+ideal_coupling = mag2db(abs(tempsim.receiver_scan(gaussfunc, 0, scanpos))) - 4.4;
 
 gaussiantransf = tempsim.calc_gaussian_phase();
 gaussianscan = sim_test(gaussiantransf, lensmodel, hornfunc, distance, scanpos, f0);
@@ -42,7 +47,7 @@ optimcirclescan = sim_test(optimtransf_circle, lensmodel, hornfunc, distance, sc
 maxpower = mag2db(max(abs(optimcirclescan)));
 maxpower = 0;
 
-gaussianscan_db = mag2db(abs(gaussianscan)) - maxpower;
+gaussianscan_db = mag2db(abs(gaussianscan)) - maxpower - 0.6;
 idealscan_db = mag2db(abs(idealscan)) - maxpower;
 optimsquarescan_db = mag2db(abs(optimsquarescan)) - maxpower;
 optimcirclescan_db = mag2db(abs(optimcirclescan)) - maxpower;
@@ -61,12 +66,16 @@ powerlin = 10 .^ (powerdB./10);
 com = sum(height .* powerlin) / sum(powerlin);
 height = height - com;
 
-
+fitgauss = fit(height, powerlin, 'gauss1');
+height = height - fitgauss.b1;
+powerdB = powerdB - pow2db(fitgauss.a1) - 4.4;
 
 p = plot(scanpos, gaussianscan_db, scanpos, idealscan_db, scanpos, optimsquarescan_db, scanpos, optimcirclescan_db, height, powerdB, scanpos, ideal_coupling);
 legend("ideal gaussian lens", "experimental","optimized lens (square)", "optimized lens (circle)", "measured", "ideal coupling");
 
-
+p(5).Marker = 'o';
+p(5).LineStyle = 'none';
+p(2).LineStyle = 'none';
 
 function [scancoupling, sim] = sim_test(lens_transformation, lensmodel, hornfunc, distance, scanpos, f0)
     sim = Simulation(lensmodel, f0, f0);
