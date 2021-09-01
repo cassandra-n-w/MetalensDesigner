4f99c0a965a4027adf0c1dd6fee2cfc0d430f105
CASSIES-COMPUTE\Cassie <cassie.whits@gmail.com>
2021-09-01 12:15:31 -0700

    Added logic to compare mag/phase of lenses of varying Er and tand.

--- Load20GHzModel.m
+++ Load20GHzModel.m
@@ -7,10 +7,16 @@ freqs = linspace(f*0.9, f*1.1, 30);
 el = Element('C:\\Users\\Cassie\\Google Drive\\Dropbox\\Documents\\My Work\\Research\\metamaterial_lens\\code\\HFSS Models\\RO3003 touchstone');
 di = Dielectric(3.00 * ones(size(freqs)), 0.001, freqs, 0.5, 1.5);
 
+di2 = Dielectric(2.95 * ones(size(freqs)), 0.001, freqs, 0.5, 1.5);
+di3 = Dielectric(2.9 * ones(size(freqs)), 0.001, freqs, 0.5, 1.5);
+di4 = Dielectric(2.7 * ones(size(freqs)), 0.001, freqs, 0.5, 1.5);
+
 proto = ProtoTL(el, di);
+proto2 = ProtoTL(el, di4);
 
 lensmodel = Lens(dim(1), layer_thickness*ones(11, 1),  zf, g, proto);
 %lensmodel = Lens(dim(1), 0.8*layer_thickness*ones(11, 1),  zf, g, proto);
+lensmodel2 = Lens(dim(1), layer_thickness*ones(11, 1),  zf, g, proto2);
 
 % bg_arr only has the first quandrant of information in it
 szbg = size(bg_arr);
@@ -33,23 +39,33 @@ for i = 1:szbg(1)
         bgtemp = bg_all(i,j,:);
         
         lensmodel.TL_array(i,j) = TL(bgtemp, lensmodel.layer_thicknesses, lensmodel.TL_prototype);
+        lensmodel2.TL_array(i,j) = TL(bgtemp, lensmodel2.layer_thicknesses, lensmodel2.TL_prototype);
         
     end
 end
 
 sgrid = lensmodel.CalcSParam(f0);
+sgrid2 = lensmodel2.CalcSParam(f0);
         
 
 %pad = zeros(1000,1000);
 
 %padded = lensmodel.PadSParams(pad, f0);
 
-phase = angle(sgrid);
+phase = angle(sgrid./sgrid(84,84));
+phase2 = angle(sgrid2./sgrid2(84,84));
 mag = abs(sgrid);
+mag2 = abs(sgrid2);
 
 %imagesc(mag);
 
-%plot(phase(84,:));
+x = 1:length(phase(84,:));
+subplot(2,1,1);
+plot(x, phase(84,:), x, phase2(84,:));
+title("Phase")
+subplot(2,1,2);
+plot(x, mag2db(mag(84,:)), x, mag2db(mag2(84,:)));
+title("Insertion Loss");
 
 %TestLensHornReal(lensmodel, f0);
 
