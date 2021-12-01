ca87f46cbfbcbbcb031819a62ba0abf6b85969b4
CASSIES-COMPUTE\Cassie <cassie.whits@gmail.com>
2021-12-01 11:58:16 -0700

    Calculates gaussian mode coupling

--- Create480GHzLens.m
+++ Create480GHzLens.m
@@ -1,6 +1,6 @@
 f = 480e9; % 480GHz central frequency
-
-freqs = linspace(f*0.9, f*1.1, 30);
+numfreqs=31;
+freqs = linspace(f*0.9, f*1.1, numfreqs);
 
 el = Element('C:\\Users\\Cassie\\My Drive\\Dropbox\\Documents\\My Work\\Research\\metamaterial_lens\\code\\HFSS Models\\Polyimide 3_4 g0p2 v99 touchstone');
 di = Dielectric(3.50 * ones(size(freqs)), 0.015, freqs, 0.005, 0.015); %layer thickness 5 - 15um, dielectric 3.5, loss tangent 0.015
@@ -26,7 +26,7 @@ ideal = sim.calc_ideal_phase();
 
 gaussapprox = sim.calc_gaussian_phase();
 
-
+% plot the ideal phase of the lens
 imagesc(sim.xvec, sim.yvec, 180+angle(ideal)*180/pi);
 xlabel("X position (mm)");
 ylabel("Y position (mm)");
@@ -35,4 +35,20 @@ title("Phase pattern over lens surface (degrees)");
 xlim([-75, 75]);
 ylim([-75, 75]);
 
+% calculate the gaussian coupling of the lens
+
+efield = @(x,y,f) (sqrt(x.^2 + y.^2) < (lensmodel.diameter/2)) * exp(0);
+% initialize incoming plane wave
+sim.initialize_E_field(efield);
+
+% transform through ideal lens pattern
+sim.transform(repmat(ideal,1,1,31));
+
+% propagate
+sim.propagate(focal_length);
+
+w0 = horn_waist;
+gaussfunc = @(x, y, f) exp(-(x.^2+y.^2)/w0^2);
+gaussfield = gaussfunc(sim.x, sim.y, f);
+coupling = sim.calculate_coupling(gaussfield);
 
