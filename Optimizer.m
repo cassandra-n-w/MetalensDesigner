67a77b6a6c38e220672374452c6dce81206635b3
CASSIES-COMPUTE\Cassie <cassie.whits@gmail.com>
2021-12-02 11:25:59 -0700

    Added optimizer class

---
+++ Optimizer.m
@@ -0,0 +1,23 @@
+classdef Optimizer
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        lensmodel
+    end
+    
+    methods
+        function obj = Optimizer(lensmodel, )
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
